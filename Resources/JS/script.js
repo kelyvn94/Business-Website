8c99ca655dac4319b89d6da7139fb7a0207add76
kelvyn94 <kmacharia584@gmail.com>
2026-01-29 19:20:30 +0300

    Initial commit: Responsive Grid System

---
+++ Resources/JS/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for contacting GreenGrow Enterprises. We will get back to you soon!");
+});
